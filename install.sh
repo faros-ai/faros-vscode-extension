c7dde12e817367db5bdce7af5975f121ca1efe20
Ron Meldiner <ron@faros.ai>
2024-09-24 11:11:48 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,32 @@
-curl -L -O https://github.com/faros-ai/faros-vscode-extension/releases/latest/download/faros-vscode-extension.vsix
-if command -v /usr/local/bin/cursor &> /dev/null
-then
-    /usr/local/bin/cursor --install-extension faros-vscode-extension.vsix
+# Function to uninstall extension
+uninstall_legacy_extension() {
+    local cmd=$1
+    if $cmd --list-extensions | grep -q "undefined_publisher.faros-vscode-extension"; then
+        echo "Uninstalling Faros legacy extension from $cmd..."
+        $cmd --uninstall-extension undefined_publisher.faros-vscode-extension
+    else
+        echo "Faros legacy extension not found in $cmd."
+    fi
+}
+
+# VSCode
+if command -v /usr/local/bin/code &> /dev/null; then
+    uninstall_legacy_extension "/usr/local/bin/code"
+    echo "Installing official Faros extension..."
+    /usr/local/bin/code --install-extension FarosAI.faros-vscode-extension
+    echo "Updating extensions..."
+    /usr/local/bin/code --update-extensions
 else
-   /usr/local/bin/code --install-extension faros-vscode-extension.vsix
+    echo "VSCode not found."
 fi
 
+# Cursor
+if command -v /usr/local/bin/cursor &> /dev/null; then
+    uninstall_legacy_extension "/usr/local/bin/cursor"
+    echo "Installing official Faros extension..."
+    /usr/local/bin/cursor --install-extension FarosAI.faros-vscode-extension
+    echo "Updating extensions..."
+    /usr/local/bin/cursor --update-extensions
+else
+    echo "Cursor not found."
+fi
